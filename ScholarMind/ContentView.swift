fdce14a9cdfb77da7a6bda40bbe476db9449dfd5
wz7in <wzqbuaa@qq.com>
2026-03-12 16:06:06 +0800

    Initial Commit

---
+++ ScholarMind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScholarMind
+//
+//  Created by 王子勤 on 2026/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScholarMindTests/ScholarMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScholarMindTests.swift
+//  ScholarMindTests
+//
+//  Created by 王子勤 on 2026/3/12.
+//
+
+import Testing
+@testable import ScholarMind
+
+struct ScholarMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
